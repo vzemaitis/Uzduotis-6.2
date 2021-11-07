04de7c4663baa61677b1fafe54ec989690fb5e3f
vzemaitis <viliuszem11@gmail.com>
2021-11-08 01:31:37 +0200

    Uzduotis #6.2

---
+++ sheet.js
@@ -0,0 +1,8 @@
+const numbers = [1, -2, 3, -4, 5];
+
+const filteredNumbers = numbers.filter(number => number > 0);
+
+console.log('-- Sukurti masyvą --')
+console.log(numbers)
+console.log('-- Išfiltruoti neigiamus skaičius --')
+console.log(filteredNumbers)
